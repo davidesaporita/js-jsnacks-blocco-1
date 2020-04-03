b03592ca80e0f341dfa75dc0c6009abb3d02523e
Davide Saporita <davide.saporita@gmail.com>
2020-04-03 17:45:41 +0200

    JS Completed

--- js/main.js
+++ js/main.js
@@ -14,99 +14,99 @@
 /******************/
 /**** JSnack 1 ****/
 /******************/
-// console.log('*** JSnack 1 in corso ***');
-// var word1 = '';
-// var word2 = '';
-// do {
-//     word1 = prompt('JSnack-1 | Inserisci la prima parola:');
-// } while (!isNaN(word1));
+console.log('*** JSnack 1 in corso ***');
+var word1 = '';
+var word2 = '';
+do {
+    word1 = prompt('JSnack-1 | Inserisci la prima parola:');
+} while (!isNaN(word1));
 
-// do {
-//     word2 = prompt('JSnack-1 | Inserisci la seconda parola:');
-// } while (!isNaN(word2));
+do {
+    word2 = prompt('JSnack-1 | Inserisci la seconda parola:');
+} while (!isNaN(word2));
 
-// console.log('Parola 1: ' + word1);
-// console.log('Parola 2: ' + word2);
-// console.log('Parola più lunga: ' + checkLength(word1, word2));
-// console.log('*** JSnack 1 completato ***');
-// console.log('---------------------------------------------------------');
+console.log('Parola 1: ' + word1);
+console.log('Parola 2: ' + word2);
+console.log('Parola più lunga: ' + checkLength(word1, word2));
+console.log('*** JSnack 1 completato ***');
+console.log('---------------------------------------------------------');
 
-// function checkLength(word1, word2) {
-//     if( word1.length > word2.length ) {
-//         return word1;
-//     } else if( word2.length > word1.length ) {
-//         return word2;
-//     } 
-//     return 'nessuna delle due';
-// }
-
-// /******************/
-// /**** JSnack 2 ****/
-// /******************/
-// console.log('*** JSnack 2 in corso ***');
-// var num;
-// do {
-//     num = parseInt(prompt('JSnack-2 | Inserisci un numero:'));
-// } while (isNaN(num));
-// console.log('Numero inserito: ' + num);
-// console.log('Numero restituito: ' + isEven(num));
-// console.log('*** JSnack 2 completato ***');
-// console.log('---------------------------------------------------------');
-
-// function isEven(num) {
-//     if (num % 2 == 0) {
-//         return num;
-//     }
-//     return ++num;
-// }
-
-// /******************/
-// /**** JSnack 3 ****/
-// /******************/
-// console.log('*** JSnack 3 in corso ***');
-
-// var names = ['Michele', 'Fabio', 'Roberto'];
-// var surnames = ['Forghieri', 'Papagni', 'Marazzini'];
-// var numInvitations = 5;
-// var fakeNames = [];
-// var x = 0; // Numero progressivo combinazioni, usato come indice di fakeNames[]
-
-// for ( i = 0 ; i<names.length ; i++ ) {
-//     for ( j = 0 ; j<surnames.length; j++ ) {
-//         fakeNames[x++] = names[i] + ' ' + surnames[j];
-//     }
-// }
-
-// var randomIndexes = [];
-// do {
-//     y = Math.floor( Math.random () * fakeNames.length);
-//     if(randomIndexes.indexOf(y) == -1) {
-//         randomIndexes.push(y);
-//     }
-// } while(randomIndexes.length < numInvitations );
-
-// console.log('Ecco la lista degli invitati dal Grande Gatsby:');
-// for(i=0 ; i<numInvitations ; i++) {
-//     console.log('Numero ' + i + ': ' + fakeNames[randomIndexes[i]]);
-// }
-// console.log('*** JSnack 3 completato ***');
-// console.log('---------------------------------------------------------');
+function checkLength(word1, word2) {
+    if( word1.length > word2.length ) {
+        return word1;
+    } else if( word2.length > word1.length ) {
+        return word2;
+    } 
+    return 'nessuna delle due';
+}
 
 /******************/
+/**** JSnack 2 ****/
+/******************/
+console.log('*** JSnack 2 in corso ***');
+var num;
+do {
+    num = parseInt(prompt('JSnack-2 | Inserisci un numero:'));
+} while (isNaN(num));
+console.log('Numero inserito: ' + num);
+console.log('Numero restituito: ' + isEven(num));
+console.log('*** JSnack 2 completato ***');
+console.log('---------------------------------------------------------');
+
+function isEven(num) {
+    if (num % 2 == 0) {
+        return num;
+    }
+    return ++num;
+}
+
+/******************/
+/**** JSnack 3 ****/
+/******************/
+console.log('*** JSnack 3 in corso ***');
+
+var names = ['Michele', 'Fabio', 'Roberto'];
+var surnames = ['Forghieri', 'Papagni', 'Marazzini'];
+var numInvitations = 5;
+var fakeNames = [];
+var x = 0; // Numero progressivo combinazioni, usato come indice di fakeNames[]
+
+for ( i = 0 ; i<names.length ; i++ ) {
+    for ( j = 0 ; j<surnames.length; j++ ) {
+        fakeNames[x++] = names[i] + ' ' + surnames[j];
+    }
+}
+
+var randomIndexes = [];
+do {
+    y = Math.floor( Math.random () * fakeNames.length);
+    if(randomIndexes.indexOf(y) == -1) {
+        randomIndexes.push(y);
+    }
+} while(randomIndexes.length < numInvitations );
+
+console.log('Ecco la lista degli invitati dal Grande Gatsby:');
+for(i=0 ; i<numInvitations ; i++) {
+    console.log('Numero ' + i + ': ' + fakeNames[randomIndexes[i]]);
+}
+console.log('*** JSnack 3 completato ***');
+console.log('---------------------------------------------------------');
+
+/**********************/
 /**** JSnack Bonus ****/
-/******************/
+/**********************/
 
 console.log('*** JSnack 4 (Bonus) in corso ***');
 var array = [];
-for( i=0 ; i<5 ; i++ ) {
-    array.push(Math.ceil(Math.random()*10));
+for( i=0 ; i<10 ; i++ ) {
+    array.push(Math.ceil(Math.random()*256));
 }
 console.log('I numeri interi generati sono: ');
 console.table(array);
-console.log('I numeri interi in posizione dispari sono: ');
+console.log('I numeri interi in posizione dispari (es index 1,3,5,7,9...) sono: ');
 sum = 0;
 for( i=0 ; i<array.length ; i++){
-    if((i+2) % 2 == 0){
+    if((i+2) % 2 != 0){
         sum += array[i];
         console.log('||| ' + array[i]);
     }
